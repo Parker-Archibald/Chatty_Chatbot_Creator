5de41996c33f043819b7bb5c9247cdee6a96a498
Parker-Archibald <parkerarchibald97@gmail.com>
2025-03-10 12:13:52 -0600

    iframe capabilities

--- middleware.ts
+++ middleware.ts
@@ -3,20 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default clerkMiddleware();
 
-const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
-const cspHeader = `
-      default-src 'self';
-      script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-      style-src 'self' 'nonce-${nonce}';
-      img-src 'self' blob: data:;
-      font-src 'self';
-      object-src 'none';
-      base-uri 'self';
-      form-action 'self';
-      frame-ancestors 'none';
-      upgrade-insecure-requests;
-  `
-
 export const config = {
     matcher: [
         // Skip Next.js internals and all static files, unless found in search params
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
+import { headers } from "next/headers";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
 };
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://parkerarchibald-qyrombw4c-parkerarchibalds-projects.vercel.app/"
+          }
+        ]
+      }
+    ]
+  }
+}
 
 export default nextConfig;
