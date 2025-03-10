438cb52730d30be2ba135129a6c30b5c4195885b
Parker-Archibald <parkerarchibald97@gmail.com>
2025-03-10 16:30:18 -0600

    iframe testing

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,19 @@ const nextConfig: NextConfig = {
 
 };
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'self' https://parkerarchibald-qyrombw4c-parkerarchibalds-projects.vercel.app/ http://localhost:3000/;
+    upgrade-insecure-requests;
+`
+
 module.exports = {
   async headers() {
     return [
@@ -13,12 +26,12 @@ module.exports = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://parkerarchibald-qyrombw4c-parkerarchibalds-projects.vercel.app/"
+            value: cspHeader.replace(/\n/g, ''),
           },
-        ]
-      }
+        ],
+      },
     ]
-  }
+  },
 }
 
 export default nextConfig;
